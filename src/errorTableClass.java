49fbaffeeb018c0d4f39f1896b3b3f474606aad3
damicoac <damicoac@gmail.com>
2010-10-04 21:12:31 -0400

    Basic template idea for class that will import the error table. All classes that import tables will have a similiar structure.

---
+++ src/errorTableClass.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+/*
+ * Error table is a flat text file. The name of this file is stored on the root directory of the code (As it is with all
+ * tables or files that need to be imported). This class has the methods to import this file into a data structure. 
+ */
+public interface errorTableClass {
+
+	/*
+	 * import the error table text file into a data structure.
+	 */
+	void importTable (File fileName);
+	
+	/*
+	 * When given an error code it returns a boolean if the error code exists in the data structure. Return is true if error code 
+	 * exists. False otherwise.
+	 */
+	Boolean doesErrorExist (String errorCode);
+	
+	/*
+	 * When given an error code this returns its associated error message.
+	 */
+	String getErrorMessage (String errorCode);
+	
+	
+}
